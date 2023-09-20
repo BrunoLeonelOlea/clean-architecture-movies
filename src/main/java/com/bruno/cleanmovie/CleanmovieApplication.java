718588fa33c6263d814fb88de52fe26c4644af7d
bolea <bolea@conexia.com>
2023-09-20 18:56:36 -0300

    first commit

---
+++ src/main/java/com/bruno/cleanmovie/CleanmovieApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.cleanmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanmovieApplication.class, args);
+	}
+
+}
